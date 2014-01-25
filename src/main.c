a23a812de9ff6a3070739a3eef3633ff8f66fa9f
Patrick M Jensen <patrick.opp@hotmail.com>
2014-01-25 21:18:50 +0100

    Tested walk_to_elem stopped on EOF - it does

--- src/main.c
+++ src/main.c
@@ -3,14 +3,17 @@
 int main(void)
 {
 	FILE *f = NULL;
-	char raw[3] = {'E', 'u', ' '};
+	char raw[3] = {'d', 'u', ' '};
 	char w[13];
 	f = fopen("./elemdb.csv", "r");
 
-	walk_to_elem(raw, f);
-	fgetc(f);
-	fgets(w, 13, f);
-	printf("%s\n", w);	
+	if (walk_to_elem(raw, f)){
+		printf("Reached end of file before match\n");
+	} else {
+		fgetc(f);
+		fgets(w, 13, f);
+		printf("%s\n", w);
+	}	
 
 	fclose(f);
 	return 0;
