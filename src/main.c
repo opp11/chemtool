268365194c7b0cb104a145851c430354cb8d83e4
Patrick M Jensen <patrick.opp@hotmail.com>
2014-01-27 12:39:38 +0100

    Can now fetch the weight of an elem as arg
    v1.0 - Fetches the weight of an element by calling
    chemtool <element>

--- src/main.c
+++ src/main.c
@@ -1,32 +1,27 @@
 #include "chemtool.h"
 
-int main(void)
+int main(int argc, char** argv)
 {
-	struct pe_elem elms[3];
-	elms[0].name[0] = 'E';
-	elms[0].name[1] = 'u';
-	elms[0].name[2] = ' ';
-	elms[1].name[0] = 'F';
-	elms[1].name[1] = 'e';
-	elms[1].name[2] = ' ';
-	elms[2].name[0] = 'U';
-	elms[2].name[1] = 'u';
-	elms[2].name[2] = 'b';
+	struct pe_elem elm;
 
-	switch(get_elem_weights(3, elms)){
-	case EFOPEN:
-		printf("Could not open elem database\n");
-		break;
-	case EENAME:
-		printf("Could not find elem\n");
-		break;
-	case EFORMAT:
-		printf("Database not formatted correctly\n");
-		break;
-	default://on success
-		printf("%f\n", elms[0].weight);
-		printf("%f\n", elms[1].weight);
-		printf("%f\n", elms[2].weight);
+	if (argc > 1){
+		elm.name[0] = argv[1][0];
+		elm.name[1] = argv[1][1] ? argv[1][1] : ' ';
+		elm.name[2] = argv[1][2] && argv[1][1] ? argv[1][2] : ' '; 
+
+		switch(get_elem_weights(1, &elm)){
+		case EFOPEN:
+			printf("Could not open elem database\n");
+			break;
+		case EENAME:
+			printf("Could not find elem\n");
+			break;
+		case EFORMAT:
+			printf("Database not formatted correctly\n");
+			break;
+		default://on success
+			printf("%f\n", elm.weight);
+		}
 	}
 
 	return 0;
