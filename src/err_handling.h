e4109b506a40885e4acfd5e70c76b8abe251749d
Patrick M Jensen <patrick.opp@hotmail.com>
2014-01-27 18:07:26 +0100

    Added comments to describe the errors

--- src/err_handling.h
+++ src/err_handling.h
@@ -3,10 +3,10 @@
 
 #include <stdio.h>
 
-#define EFOPEN  1
-#define EENAME  2
-#define EDBFMT  3
-#define EARGFMT 4
+#define EFOPEN  1 //Could not open file
+#define EENAME  2 //Could not find element
+#define EDBFMT  3 //Data is formatted wrong
+#define EARGFMT 4 //Argument is formatted wrong
 
 void print_err(int err);
 
