effaa5a1d6d7334ecdda1ff077417981decf96a2
Patrick M Jensen <patrick.opp@hotmail.com>
2014-02-22 22:52:24 +0100

    Added printing of individual elems in gui
    As of now we use a monospaced font to get the alignment
    right. This however does not look good when the window
    is resized and should be changed to a table or similar.

--- src/gui.py
+++ src/gui.py
@@ -11,7 +11,7 @@ class Win(QtGui.QWidget):
 
     def build_gui(self):
         #set up window
-        self.resize(300, 250)
+        self.resize(350, 300)
         self.setWindowTitle('Chemtool')
 
         global_grid = QtGui.QGridLayout()
@@ -26,7 +26,7 @@ class Win(QtGui.QWidget):
         self.setSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
         self.calc_btn.clicked.connect(self.process_input)
         global_grid.addWidget(self.calc_btn, 2, 0, 1, 3,
-		alignment=QtCore.Qt.AlignCenter | QtCore.Qt.AlignTop)
+		alignment=QtCore.Qt.AlignCenter | QtCore.Qt.AlignCenter)
 
         #set up the output of the total molar mass
         self.totmm_lbl = QtGui.QLabel('Total Molar Mass:', self)
@@ -34,6 +34,7 @@ class Win(QtGui.QWidget):
 
         self.totmm_box = QtGui.QLineEdit(self)
         self.totmm_box.setReadOnly(True)
+        self.totmm_box.setAlignment(QtCore.Qt.AlignRight)
         global_grid.addWidget(self.totmm_box, 3, 1)
 
 	self.mm_unit_lbl = QtGui.QLabel('g/mol', self)
@@ -43,17 +44,22 @@ class Win(QtGui.QWidget):
         self.elem_list = QtGui.QListWidget(self)
         self.elem_list.setSizePolicy(QtGui.QSizePolicy.Expanding,
 		QtGui.QSizePolicy.Expanding)
+        self.elem_list.setFont(QtGui.QFont('Monospace', 10))
         global_grid.addWidget(self.elem_list, 4, 0, 1, 3)
 
         #setup the layout of the grid
         global_grid.setRowMinimumHeight(0, 30)
+        global_grid.setRowMinimumHeight(2, 40)
         self.setLayout(global_grid)
 
     def process_input(self, event):
+        self.elem_list.clear()
         elems = chemtool.get_elem_data(str(self.input_box.text()))
         total_mm = 0.0
         for (sname, lname, quant, weight) in elems:
              total_mm += weight * quant
+             self.elem_list.addItem(
+             	"{0}{1:>6}    {2:>10.7f}    {3}".format(sname, quant, weight, lname))
         self.totmm_box.setText("{:.7f}".format(total_mm))
 
 if __name__ == '__main__':
